fc03a7eab646009e77a8b82c5e7883c8c0086eab
Floha <floha2011@hotmail.de>
2021-03-12 16:39:10 +0100

    Initial goal list WIP

---
+++ ssr-goals.js
@@ -0,0 +1,105 @@
+const bingoLists = {normal:[
+    [
+        {
+            "name": "Get Arrows from a Goddess Wall",
+            "types": [
+                "GoddessSword",
+				"Dungeons",
+				"Skyview",
+				"Bow"
+            ]
+        },
+		{
+            "name": "Get Bombs from a Goddess Wall",
+            "types": [
+                "GoddessSword",
+				"Dungeons",
+				"Skyview",
+				"Bombs"
+            ]
+        },
+		{
+            "name": "Drink one of every Potion",
+            "types": [
+                "Potions",
+				"Dungeons",
+				"LMF",
+				"Bottles"
+            ]
+        },
+		{
+            "name": "5 Goddess Cubes in any Region",
+            "types": [
+                "GoddessSword",
+				"Faron",
+				"Eldin",
+				"Lanayru"
+            ]
+        },
+		{
+            "name": "All Bird Statues in any Region",
+            "types": [
+                "GoddessSword",
+				"Dungeons",
+				"Skyview",
+				"Bow"
+            ]
+        },
+		{
+            "name": "Duplicate an Ampilus Egg",
+            "types": [
+                "Lanayru"
+            ]
+        },
+		{
+            "name": "Clear 2 Dungeons",
+            "types": [
+                "Dungeons"
+            ]
+        },
+		{
+            "name": "Complete any Silent Realm",
+            "types": [
+                "Harp",
+            ]
+        },
+		    "name": "28+ cuts at Bamboo Island",
+            "types": [
+                "Minigames",
+				"Sword"
+            ]
+        },
+		    "name": "Complete any Minigame",
+            "types": [
+                "Minigames"
+            ]
+        },
+		    "name": "Go through the Gate of Time",
+            "types": [
+                "Mastersword",
+				"Sword"
+            ]
+        },
+		    "name": "10 Hearts",
+            "types": [
+                "Hearts"
+            ]
+        },
+		    "name": "3 Bottles",
+            "types": [
+                "Bottles"
+            ]
+        },
+		    "name": "Clear 2 Song Checks",
+            "types": [
+                "Harp",
+				"Thunderhead"
+            ]
+        },
+		    "name": "50 Gratitude Crystals",
+            "types": [
+                "Crystals"
+            ]
+        },
+    ]
+]};
