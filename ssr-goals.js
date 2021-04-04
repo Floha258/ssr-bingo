10ebba0b5fcded8b3644005d0052b6cf162012b6
harmjan387 <47255079+harmjan387@users.noreply.github.com>
2021-04-04 15:35:07 +0200

    Update ssr-goals.js
    Added goals

--- ssr-goals.js
+++ ssr-goals.js
@@ -99,7 +99,180 @@ const bingoLists = {normal:[
 		    "name": "50 Gratitude Crystals",
             "types": [
                 "Crystals"
-            ]
+            ] 
         },
-    ]
-]};
+		    "name": "Kill Girahim 1 with a dart",
+            "types": [
+                "Dungeons"
+		"Skyview"
+            ] 
+        },
+		    "name": "Sacred Water",
+            "types": [
+                "Dungeons"
+	        "Skyview"
+		'Bottles"
+            ] 
+        },
+		    "name": "Pick up a Heart",
+            "types": [
+                "Heart Medal"	   
+            ] 
+        },	
+		    "name": "Find an inventory expansion",
+            "types": [
+                "Misc"
+            ] 
+        },
+		    "name": "Bug Heaven in under 1m30",
+            "types": [
+                "minigames"
+            ] 
+        },
+		    "name": "12 Hearts",
+            "types": [
+                "Hearts"
+            ] 
+        },
+		    "name": "Cursed Medal",
+            "types": [
+                "Misc"
+            ] 
+        },
+		    "name": "Large Inventory Expansion",
+            "types": [
+                "Misc"
+            ] 
+        },
+		    "name": "5 Lanayru Goddess Cubes",
+            "types": [
+                "Goddess Sword"
+	        "Cubes"
+            ] 
+        },
+		    "name": "5 Eldin Goddess Cubes",
+            "types": [
+                "Goddess Sword"
+		"Cubes"
+            ] 
+        },
+		    "name": "5 Faron Goddess Cubes",
+            "types": [
+                "Goddess Sword"
+		"Cubes"
+            ] 
+        },
+		    "name": "Beat an unrequired Trial",
+            "types": [
+                "Harp"
+            ] 
+        },
+		    "name": "Full Clear a Dungeon",
+            "types": [
+                "Dungeons"
+		    "Goddess Sword"
+		    "Bow"
+            ] 
+        },
+		    "name": "Perform an Extending Blow",
+            "types": [
+                "Sword"
+		"Slingshot"
+            ] 
+        },
+		    "name": "Clear 2 Silent Realms",
+            "types": [
+                "Goddess Harp"
+		    "Trials"
+            ] 
+        },
+		    "name": "60 Graditude Crystals",
+            "types": [
+                "Crystals"
+            ] 
+        },
+		    "name": "70 Gratitude Crystals",
+            "types": [
+                "Crystals"
+            ] 
+        },
+		    "name": "80 Gratitude Crystals",
+            "types": [
+                "Crystals"
+            ] 
+        },
+		    "name": "Stone of Trials",
+            "types": [
+                "Misc"
+            ] 
+        },
+		    "name": "3 Golden Rupees",
+            "types": [
+                "Rupees"
+            ] 
+        },
+		    "name": "5 Golden Rupees",
+            "types": [
+                "Rupees"
+            ] 
+        },
+		    "name": "5 Silver Rupees",
+            "types": [
+                "Rupees"
+            ] 
+        },
+		    "name": "5 Earth Temple Keypieces",
+            "types": [
+                "Dungeons"
+		"Keys"
+            ] 
+        },
+		    "name": "Complete Fun Fun Island",
+            "types": [
+                "Minigames"
+		"Lanayru"
+            ] 
+        },
+		    "name": "50 Gratitude Crystals",
+            "types": [
+                "Crystals"
+            ] 
+        },
+		    "name": "20 Bird Statues",
+            "types": [
+                "Faron"
+		"Eldin"
+		"Lanayru"
+		"Statues"
+            ] 
+        },
+		    "name": "All Sandsea Bird Statues",
+            "types": [
+                "Statues"
+	        "Lanayru"
+                "Clawshots"
+		"Statues"
+            ] 
+        },
+		    "name": "Turn a Windmill",
+            "types": [
+                "Bellows"
+            ] 
+        },
+		    "name": "4 Bottles",
+            "types": [
+                "Bottles"
+            ] 
+        },
+		    "name": "3 Bottles",
+            "types": [
+                "Bottles"
+            ] 
+        },
+		    "name": "Upgrade 1 B-Wheel item",
+            "types": [
+                "Misc"
+            ] 
+        },
+     ]
+  };
